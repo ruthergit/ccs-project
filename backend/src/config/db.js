0cb350bdaacd1dc82f9635003096b4bb59c70508
Ruther <ruthermrcs@gmail.com>
2026-04-06 10:17:07 +0800

    added ssl

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,6 +10,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false // This allows the connection to Aiven
+  }
 });
 
 export default pool;
